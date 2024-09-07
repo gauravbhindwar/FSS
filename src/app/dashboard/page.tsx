7ce7af53f867be578381f7353157e10c54d60a1a
Shivank Goel <shivankgoel600@gmail.com>
2024-09-07 20:45:22 +0530

    ui of dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import  SidebarDemo  from "@/components/Base/Header/sidebar"; 
+//Options for what a user can do
+//1. Buttons to redirect to pages to fill form
+//2. Check Form status -- pending confirmed rejected
+//3. Can check the data he/she has filled
+//4. can take printout in form of pdf
+//5. 
+const page = () => {
+
+  return <div className="w-[100vw] h-[100vh]">
+    <SidebarDemo />
+    </div>;
+};
+
+export default page;
