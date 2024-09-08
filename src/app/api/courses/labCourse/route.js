92dbfa8e41413eb3d82d4b65b2dd350b4d3cba55
Gaurav Bhindwar <bhindwargaurav@gmail.com>
2024-09-08 05:32:21 +0530

    successfully fetched courses data from database according to course classification

---
+++ src/app/api/courses/labCourse/route.js
@@ -0,0 +1,27 @@
+import { connect } from "../../../helper/dbConfig"; // Adjust the path to your db module
+import { Course } from "../../../../lib/dbModels/dbModels";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function POST(req) {
+  await connect();
+  try {
+    const body = await req.json();
+    const { courseClassification } = body;
+
+    console.log(courseClassification);
+
+    const courses = await Course.find({
+      courseClassification: courseClassification,
+    }); // Fetch data from the database
+
+    console.log(courses);
+
+    return NextResponse.json({ courses }, { status: 200 });
+  } catch (error) {
+    console.error("Error getting Lab Data:", error);
+    return NextResponse.json(
+      { message: "Error Getting Lab data" },
+      { status: 500 }
+    );
+  }
+}
