2ca22873f14a77fd840defa109be8318dd4356ec
Gaurav Bhindwar <bhindwargaurav@gmail.com>
2024-09-15 17:54:49 +0530

    minor changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 import UserDashboard from "@/components/Base/Header/sidebar";
 const page = () => {
   const cookieStore = cookies().getAll(); // Convert to array
-  console.log(cookieStore);
+  // console.log(cookieStore);
   return (
     <div className="w-[100vw] h-[100vh]">
       <UserDashboard cookies={cookieStore} />
